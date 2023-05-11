45f9fb8847d1aae6180912d1619811a72d4bb4aa
marioP2000 <132082343+marioP2000@users.noreply.github.com>
2023-05-11 16:47:29 +0200

    Delete Bootstrap Navbar with animated sidebar v2 directory

--- Bootstrap Navbar with animated sidebar v2/main.js
+++
@@ -1,7 +0,0 @@
-//Open sidebar
-const sidebarButton = document.querySelector(".fa-bars");
-const sidebarWindow = document.querySelector(".sidebar");
-
-sidebarButton.onclick = () => {
-    sidebarWindow.classList.toggle("showSidebar");
-}
